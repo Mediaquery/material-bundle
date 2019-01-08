89bfcdac1133ee1951ef9963413a4fdd13dc2c4a
Jérôme Lewis Metz <jeromelewis@gmx.de>
2019-01-08 07:23:15 +0100

    0.0.2

--- tests/ContaoMaterialBundleTest.php
+++ tests/ContaoMaterialBundleTest.php
@@ -8,7 +8,7 @@
  * @license LGPL-3.0-or-later
  */
 
-namespace Contao\MaterialBundle\Tests;
+namespace Mediaquery\MaterialBundle\Tests;
 
 use Mediaquery\MaterialBundle\ContaoMaterialBundle;
 use PHPUnit\Framework\TestCase;
